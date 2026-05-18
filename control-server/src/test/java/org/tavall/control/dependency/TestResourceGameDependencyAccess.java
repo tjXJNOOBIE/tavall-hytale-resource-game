34c5cbf29f99d48a5b22ee23e6ad8e4393250837
TJ <tjxjnoobie@gmail.com>
2026-05-18 13:33:57 -0700

    minecraft-main: fix control-server dependency test helper

---
+++ control-server/src/test/java/org/tavall/control/dependency/TestResourceGameDependencyAccess.java
@@ -0,0 +1,9 @@
+package org.tavall.control.dependency;
+
+import org.tavall.control.bootstrap.IGlobalDependencyAccess;
+
+/**
+ * Test-only dependency access for the repo-local DI layer.
+ */
+public final class TestResourceGameDependencyAccess implements IGlobalDependencyAccess {
+}
